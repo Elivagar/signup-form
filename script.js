9cb42be837114c4c4524b2e04bdfb2afde918ada
Helgrindi <v.decoen@gmail.com>
2025-01-17 14:35:53 +0100

    Added form success message, fixed some remaining bugs.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const inputs = document.querySelectorAll('input[required]');
+const inputs = document.querySelectorAll('input');
 const password = document.getElementById('password');
 const repeatPassword = document.getElementById('repeat-password');
-const invitationCode = document.getElementById('invitation-code');
 
+const invitationCode = document.getElementById('invitation-code');
 const validCodes = ['TROMSØ123', 'TROMSØ456', 'TROMSØ789'];
+
+const logo = document.querySelector(".logo");
+const hero = document.querySelector(".hero");
+const form = document.getElementById("dummyForm");
+const runeCircles = document.querySelector(".rune-circles");
+const gradient = document.querySelector(".gradient");
+const main = document.querySelector("main");
+
+
+// >>> FORM VALIDATION <<<
 
 inputs.forEach(input => {
     let interacted = false;
 
+    // DELETES ERROR WHEN EXITING EMPTY FIELD
     input.addEventListener('input', () => {
         interacted = true;
         if (input.validity.valid) {
@@ -28,6 +39,7 @@ inputs.forEach(input => {
     });
 });
 
+// PASSWORD CHECK
 repeatPassword.addEventListener('input', () => {
     if (repeatPassword.value === password.value) {
         repeatPassword.setCustomValidity('');
@@ -47,78 +59,114 @@ repeatPassword.addEventListener('blur', () => {
     }
 });
 
-repeatPassword.addEventListener('input', () => {
-    if (repeatPassword.value === password.value) {
-        repeatPassword.classList.add('valid');
-        repeatPassword.classList.remove('invalid');
-    } else {
-        repeatPassword.classList.add('invalid');
-        repeatPassword.classList.remove('valid');
-    }
+// INVITATION CODE CHECK
+// invitationCode.addEventListener('input', () => {
+//     if (validCodes.includes(invitationCode.value.trim())) {
+//         invitationCode.setCustomValidity('');
+//         invitationCode.classList.add('valid');
+//         invitationCode.classList.remove('invalid');
+//     } else {
+//         invitationCode.setCustomValidity('Invalid code, contact me');
+//         if (invitationCode.value.trim() !== '') {
+//             invitationCode.classList.add('invalid');
+//             invitationCode.classList.remove('valid');
+//         } else {
+//             invitationCode.classList.remove('invalid', 'valid'); // Reset if empty
+//         }
+//     }
+// });
+
+// invitationCode.addEventListener('blur', () => {
+//     if (invitationCode.value.trim() === '') {
+//         invitationCode.setCustomValidity('');
+//         invitationCode.classList.remove('valid', 'invalid');
+//     }
+// });
+
+
+// >>> FORM SUBMIT MESSAGE <<<
+
+const successMessage = document.createElement("div");
+let submitted = false;
+successMessage.classList.add("success-message");
+// successMessage.innerHTML = "<p>YOU'RE ALL SET, WELCOME!</p>";
+successMessage.innerHTML = "<p>DUMMY FORM SUBMITTED, CLICK LOGIN TO RETURN TO FORM</p>";
+successMessage.innerHTML = successMessage.innerHTML + "<button>LOG IN</button>";
+main.appendChild(successMessage);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitted = true;
+
+    form.style.opacity = "0";
+    form.style.pointerEvents = "none";
+
+    successMessage.style.pointerEvents = "auto";
+    successMessage.style.opacity = "1";
 });
 
-invitationCode.addEventListener('input', () => {
-    if (validCodes.includes(invitationCode.value.trim())) {
-        invitationCode.setCustomValidity('');
-        invitationCode.classList.add('valid');
-        invitationCode.classList.remove('invalid');
-    } else {
-        invitationCode.setCustomValidity('Invalid code, contact me');
-        if (invitationCode.value.trim() !== '') {
-            invitationCode.classList.add('invalid');
-            invitationCode.classList.remove('valid');
-        } else {
-            invitationCode.classList.remove('invalid', 'valid'); // Reset if empty
-        }
-    }
-});
+const loginBtn = document.querySelector(".success-message button");
 
-invitationCode.addEventListener('blur', () => {
-    if (invitationCode.value.trim() === '') {
-        invitationCode.setCustomValidity('');
-        invitationCode.classList.remove('valid', 'invalid');
-    }
+loginBtn.addEventListener("click", () => {
+    submitted = false;
+
+    successMessage.style.opacity = "0";
+    successMessage.style.pointerEvents = "none";
+
+    form.style.opacity = "1";
+    form.style.pointerEvents = "auto";
+    form.reset();
+    inputs.forEach(input => {
+        input.classList.remove('valid', 'invalid');
+    });
 });
 
 
 // >>> LOGO HOVER ANIMATION <<<
 
+const journeyHome = document.createElement("div");
+journeyHome.classList.add("journey-home");
+journeyHome.textContent = "JOURNEY HOME";
+main.appendChild(journeyHome);
 
 document.addEventListener("DOMContentLoaded", () => {
-    const logo = document.querySelector(".logo");
-    const hero = document.querySelector(".hero");
-    const form = document.querySelector("form");
-    const runeCircles = document.querySelector(".rune-circles");
-    const gradient = document.querySelector(".gradient");
-    const main = document.querySelector("main");
-
-    const journeyHome = document.createElement("div");
-    journeyHome.classList.add("journey-home");
-    journeyHome.textContent = "JOURNEY HOME";
-    main.appendChild(journeyHome);
-
-    // >>> HOVER ENTER <<<
+    // HOVER ENTER
     logo.addEventListener("mouseenter", () => {
+        if (submitted) {
+            successMessage.style.opacity = "0";
+            successMessage.style.transition = "opacity 400ms ease-in-out";
+        }
+
         hero.style.opacity = "0";
+        hero.style.pointerEvents = "none";
         form.style.opacity = "0";
+        form.style.pointerEvents = "none";
         gradient.style.opacity = "0";
         
-        runeCircles.style.animationName = "spin-expand";
-        runeCircles.style.animationDuration = "1600ms";
-        runeCircles.style.animationTimingFunction = "ease-in-out";
-        runeCircles.style.animationDirection = "forwards";
+        runeCircles.style.animation = "spin-expand 1600ms ease-in-out forwards"
         runeCircles.style.animationFillMode = "forwards";
 
-        journeyHome.style.opacity = "1";
+        journeyHome.style.pointerEvents = "auto";
+        journeyHome.style.opacity = "0.64";
     });
 
+    // HOVER LEAVE
     logo.addEventListener("mouseleave", () => {
+        if (submitted) {
+            successMessage.style.opacity = "1";
+            successMessage.style.transition = "opacity 800ms ease-in-out 400ms"
+        } else {
+            form.style.opacity = "1";
+            form.style.pointerEvents = "auto";
+        }
+
         hero.style.opacity = "1";
-        form.style.opacity = "1";
+        hero.style.pointerEvents = "auto";
         gradient.style.opacity = "1";
 
         runeCircles.style.animation = "spin-shrink 1600ms ease-in-out forwards";
 
+        journeyHome.style.pointerEvents = "none";
         journeyHome.style.opacity = "0";
     });
 });
